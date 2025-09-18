c024242ee1e3d3b97ad91d4605796163e39f35ad
ReshmaMahadik0 <mahadikreshma408@gmail.com>
2025-09-18 19:35:41 +0530

    commit the changes in class demo1

---
+++ src/Demo1.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Demo1 {
+
+
+    public static void main(String[] args) {
+
+        List<Integer> list = Arrays.asList(10, 15, 8, 49, 25, 98, 98, 32, 15);
+        long count = list.stream()
+                .count();
+        System.out.println(count);
+
+    }
+}
